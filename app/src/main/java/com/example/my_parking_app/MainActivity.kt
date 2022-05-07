f4eb85b7ba150c1f507899d0eb5b8cbee47f77bd
juandraper <e146613@gmail.com>
2022-05-07 17:28:40 -0300

    adding base for project

---
+++ app/src/main/java/com/example/my_parking_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.my_parking_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
