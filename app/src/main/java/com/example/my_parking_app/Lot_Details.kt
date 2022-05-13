6cc4ee3708eb3d7bcd8a6195a44c817494eb9696
juandraper <e146613@gmail.com>
2022-05-13 14:55:21 -0300

    nearly the final cuts of main activity

---
+++ app/src/main/java/com/example/my_parking_app/Lot_Details.kt
@@ -0,0 +1,11 @@
+package com.example.my_parking_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Lot_Details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lot_details)
+    }
+}
---
+++ app/src/main/java/com/example/my_parking_app/OnItemClicked.kt
@@ -0,0 +1,7 @@
+package com.example.my_parking_app
+
+interface OnItemClicked{
+        fun onLotClick(position: Int){
+
+        }
+}
